f4f79eec8444ce2fb03dd6c79d2fe0da06d2557b
Aleksandar Minchev <aminchev1988@gmail.com>
2025-01-21 22:45:37 +0200

    Creating dynamic details page functionality

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import { findMovie } from "../services/movieService.js";
 
 const movieController = Router();
 
 movieController.get('/create', (req, res) => {
     res.render('create')
+});
+movieController.get('/movies/:movieId/details', (req, res) => {
+    const movieId = req.params.movieId;
+    const movie = findMovie(movieId);    
+
+    res.render('details', { movie });
 });
 
 export default movieController;
