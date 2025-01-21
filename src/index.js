6a0b6117707f0af0c0825c98b0e3f9ab33fa3e13
Aleksandar Minchev <aminchev1988@gmail.com>
2025-01-21 21:30:01 +0200

    Adding Home controller

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+import homeController from './controllers/homeController.js';
 
 
 const app = express();
@@ -12,15 +13,10 @@ app.set('views', './src/views');
 
 app.use('/static', express.static('src/public'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-app.get('/about', (req, res) => {
-    res.render('about');
-});
+app.use(homeController);
+
 app.get('*', (req, res) => {
     res.render('404');
-})
-
+});
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
