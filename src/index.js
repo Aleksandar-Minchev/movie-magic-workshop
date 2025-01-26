9bd80966b982482e1ef050fb4fe3304f1ba80bd2
Aleksandar Minchev <aminchev1988@gmail.com>
2025-01-26 20:36:08 +0200

    Setup mongoose

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+import mongoose from 'mongoose';
+
 import routes from './routes.js';
 import showRating from './helpers/ratingHelper.js';
 
 
+
 const app = express();
 
+//db config
+try {
+    const uri = 'mongodb://localhost:27017/movie-magic-workshop'
+    await mongoose.connect(uri);
+
+    console.log('DB connected successfully');  
+    
+} catch (err) {
+    console.error(err.message)
+}
+
+
+//handlebars config
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
     helpers: {
@@ -15,9 +31,13 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
+
+//express config
 app.use('/static', express.static('src/public'));
 app.use(express.urlencoded({extended: false})); //Learn express to parse form data (on POST request)
 
+//setup routes
 app.use(routes);
 
+//start server
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
