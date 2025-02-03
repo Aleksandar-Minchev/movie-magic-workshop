113a4001524091b3db1a83572cc26184886fee20
Aleksandar Minchev <aminchev1988@gmail.com>
2025-02-03 16:12:14 +0200

    Adding Login functionality

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,32 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const SECRET = '$2b$10$RzfxbyasdfasfqLFEPgPbqvJuVMZIx1iPlkwPIG72NfLlkuREs6'
 
 export default {
     register(userData){
         return User.create(userData)      
+    },
+    async login(email, password){
+        //Check if user exists in DB
+        const user = await User.findOne({email});
+        if (!user){
+            throw new Error('Invalid email or password!')
+        }
+
+        //Check if password is correct
+        const isValidPass = await bcrypt.compare(password, user.password);
+        if (!isValidPass){
+            throw new Error('Invalid email or password!');
+        }
+        //Generate token
+        const payload = {
+            id: user.id,
+            email: user.email
+        }
+        const token = jwt.sign(payload, SECRET, {expiresIn: '2h'})
+                
+        return token;
     }
 }
